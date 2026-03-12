ff1167bf8bcdb6db106d2bfaac160de576c6aa4b
a8anassis <a8anassis@gmail.com>
2026-03-12 20:24:05 +0200

    chore: add POCO (Plain Old CLR Class) classes with public properties, constructors and Equals, GetHashCode and ToString

---
+++ PocoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PocoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student alice = new Student();
+            Student bob = new();    // C# 9
+            var julia = new Student(); // C# 10
+
+            Teacher teacher = new(1, "Μάκης", "Καπέτης");   // Overloaded Constructor
+            Teacher teacher2 = new() { Id = 2, Firstname = "Γιάννης", Lastname = "Παπαδόπουλος" }; // Object Initializer
+
+            // Student student = new() { Id = 1, Firstname = "Alice", Lastname = "Smith" };
+        }
+    }
+}
